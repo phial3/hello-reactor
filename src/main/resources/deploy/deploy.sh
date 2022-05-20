ed82786fa72fe4f2da390883960a910a8d02267d
gaoyanfei3 <gaoyanfei3@jd.com>
2022-05-20 22:32:57 +0800

    add deploy

---
+++ src/main/resources/deploy/deploy.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+############################################################
+# Deploy application
+# Copy new jar file and backup jar file
+# @author mayanjun
+# @Email mayanjun@jd.com
+# @ERP mayanjun3
+############################################################
+
+# init app
+
+# custom scp home
+SCP_HOME="/Users/mayanjun/Desktop/dddddddddd/scp"
+
+BIN_DIR="$(cd `dirname $0`; pwd)"
+APP_DIR="`dirname $BIN_DIR`"
+APP_JAR_FILE="@maven.project.name@-@maven.project.version@.jar"
+ROLLBACK_DIR="$APP_DIR/ROLLBACK"
+
+# backup apps
+if [[ ! -d "$ROLLBACK_DIR" ]]; then
+	mkdir -p $ROLLBACK_DIR
+fi
+
+MAX_VERSION="`ls -1 $ROLLBACK_DIR |sort -nr|head -n1`"
+
+if [[ -n "$MAX_VERSION" ]]; then
+	echo "Max version: $MAX_VERSION"
+else
+	MAX_VERSION=0
+fi
+
+BACKUP_VERSION=$(($MAX_VERSION+1))
+BACKUP_DIR="$ROLLBACK_DIR/$BACKUP_VERSION"
+
+if [[ ! -f "$SCP_HOME/$APP_JAR_FILE" ]]; then
+	echo "Deploy fail: New app file dose not exists: $SCP_HOME/$APP_JAR_FILE"
+	exit 1
+fi
+
+# stop app first
+$BIN_DIR/stop.sh
+
+# mv current file to BACKUP_DIR
+if [[ -f "$APP_DIR/$APP_JAR_FILE" ]]; then
+	if [[ ! -d "$BACKUP_DIR" ]]; then
+		mkdir -p $BACKUP_DIR
+	fi
+	echo "Backup version: $BACKUP_VERSION"
+
+	mv $APP_DIR/$APP_JAR_FILE $BACKUP_DIR
+	NOW_TIME=`date "+%Y-%m-%d %H:%M:%S"`
+	echo "$NOW_TIME" > $BACKUP_DIR/backup-time
+else
+	echo "WARN: app file dose not exists and backup will be ignored: $APP_DIR/$APP_JAR_FILE"
+fi
+
+mv $SCP_HOME/$APP_JAR_FILE $APP_DIR
+
+
+# start app
+$BIN_DIR/start.sh
+
+
+exit 0
---
+++ src/main/resources/deploy/rollback.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+############################################################
+# Rollback application
+# Copy new jar file and backup jar file
+# @author mayanjun
+# @Email mayanjun@jd.com
+# @ERP mayanjun3
+############################################################
+
+# init app
+
+# custom scp home
+SCP_HOME="/Users/mayanjun/Desktop/dddddddddd/scp"
+
+BIN_DIR="$(cd `dirname $0`; pwd)"
+APP_DIR="`dirname $BIN_DIR`"
+APP_JAR_FILE="@maven.project.name@-@maven.project.version@.jar"
+ROLLBACK_DIR="$APP_DIR/ROLLBACK"
+
+STEP="$1"
+
+if [[ -z $STEP ]]; then
+	echo "ERROR: The step is not specified"
+	echo "Usage rollback n"
+	exit 1
+fi
+
+if [[ $STEP -gt 0 ]]; then
+	echo "Rollback $STEP step(s)"
+else
+	echo "ERROR: The step is must be a positive integer and great than 0"
+	echo "Usage rollback n"
+	exit 2
+fi
+
+# check
+if [[ ! -d "$ROLLBACK_DIR" ]]; then
+	echo "Rollback fail: The rollback dir does not exists: $ROLLBACK_DIR"
+	exit 3	
+fi
+
+BACKUP_VERSION="`ls -1 $ROLLBACK_DIR |sort -nr|head -n $STEP|tail -n1`"
+
+BACKUP_DIR=""
+
+if [[ -n "$BACKUP_VERSION" ]]; then
+	BACKUP_DIR="$ROLLBACK_DIR/$BACKUP_VERSION"
+else
+	echo "Rollback fail: The ROLLBACK dir is empty: $ROLLBACK_DIR"
+	exit 3
+fi
+
+if [[ ! -f "$BACKUP_DIR/$APP_JAR_FILE" ]]; then
+    echo "Rollback fail: The ROLLBACK file does not exists: $ROLLBACK_DIR"
+    exit 3
+fi
+
+# stop app first
+$BIN_DIR/stop.sh
+
+# remove current version
+rm -rf $APP_DIR/$APP_JAR_FILE
+cp $BACKUP_DIR/$APP_JAR_FILE $APP_DIR
+echo "Backup to version $BACKUP_VERSION (`cat $BACKUP_DIR/backup-time`)"
+
+# start app
+$BIN_DIR/start.sh
+
+exit 0
